fb1c3890aed2e3db5581ee5f21a66e23f8d7c733
Jenna Castillo <jennacodes101@gmail.com>
2022-07-19 22:32:31 +0200

    seeded seed.rb also updated model relationships -- need to do this to test

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,6 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
 ActiveRecord::Schema.define(version: 2022_07_16_150703) do
 
   # These are extensions that must be enabled in order to support this database
@@ -57,7 +56,6 @@ ActiveRecord::Schema.define(version: 2022_07_16_150703) do
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
-
 
   add_foreign_key "bookings", "excursions"
   add_foreign_key "bookings", "users"
