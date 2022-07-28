b6552114be1e112bc51ca0517473c29c88bf9b5c
JoÃao Bernardo <joaogoncalob@gmail.com>
2022-07-28 23:21:49 +0100

    almost there

--- app/javascript/controllers/mapbox_controller.js
+++ app/javascript/controllers/mapbox_controller.js
@@ -1,29 +1,74 @@
+// import { Controller } from "@hotwired/stimulus"
+// import mapboxgl from "mapbox-gl"
+// import MapboxGeocoder from "@mapbox/mapbox-gl-geocoder"
+
+// export default class extends Controller {
+//   static values = {
+//     apiKey: String,
+//     markers: Array
+//   }
+
+//   connect() {
+//     mapboxgl.accessToken = this.apiKeyValue
+
+//     this.map = new mapboxgl.Map({
+//       container: this.element,
+//       style: "mapbox://styles/mapbox/satellite-streets-v11",
+//       zoom: 1.5,
+//       center: [30, 50],
+//       projection: 'globe'
+//     })
+
+//     this.#addMarkersToMap()
+//     this.#fitMapToMarkers()
+
+//     this.map.addControl(new MapboxGeocoder({ accessToken: mapboxgl.accessToken,
+//       mapboxgl: mapboxgl }))
+//   }
+
+//   #addMarkersToMap() {
+//     this.markersValue.forEach((marker) => {
+//       const popup = new mapboxgl.Popup().setHTML(marker.info_window)
+
+//       // Create a HTML element for your custom marker
+//       const customMarker = document.createElement("div")
+//       customMarker.className = "marker"
+//       customMarker.style.backgroundImage = `url('${marker.image_url}')`
+//       customMarker.style.backgroundSize = "contain"
+//       customMarker.style.width = "25px"
+//       customMarker.style.height = "25px"
+
+//       new mapboxgl.Marker(customMarker)
+//         .setLngLat([ marker.lng, marker.lat ])
+//         .setPopup(popup)
+//         .addTo(this.map)
+//     });
+//   }
+
+//   #fitMapToMarkers() {
+//     const bounds = new mapboxgl.LngLatBounds()
+//     this.markersValue.forEach(marker => bounds.extend([ marker.lng, marker.lat ]))
+//     this.map.fitBounds(bounds, { padding: 70, maxZoom: 15, duration: 0 })
+//   }
+// }
 import { Controller } from "@hotwired/stimulus"
 import mapboxgl from "mapbox-gl"
-import MapboxGeocoder from "@mapbox/mapbox-gl-geocoder"
-
 export default class extends Controller {
   static values = {
     apiKey: String,
     markers: Array
   }
-
   connect() {
     mapboxgl.accessToken = this.apiKeyValue
-
     this.map = new mapboxgl.Map({
-      container: this.element,
-      style: "mapbox://styles/mapbox/satellite-streets-v11",
-      zoom: 1.5,
-      center: [30, 50],
-      projection: 'globe'
-    })
-
+        container: this.element,
+        style: "mapbox://styles/mapbox/satellite-streets-v11",
+        zoom: 1.5,
+        center: [30, 50],
+        projection: 'globe'
+      })
     this.#addMarkersToMap()
     this.#fitMapToMarkers()
-
-    this.map.addControl(new MapboxGeocoder({ accessToken: mapboxgl.accessToken,
-      mapboxgl: mapboxgl }))
   }
 
   #addMarkersToMap() {
@@ -39,9 +84,9 @@ export default class extends Controller {
       customMarker.style.height = "25px"
 
       new mapboxgl.Marker(customMarker)
-        .setLngLat([ marker.lng, marker.lat ])
-        .setPopup(popup)
-        .addTo(this.map)
+      .setLngLat([ marker.lng, marker.lat ])
+      .setPopup(popup)
+      .addTo(this.map)
     });
   }
 
